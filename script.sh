be885d848a7b7808b94aa07ea87632331e861c4e
Ahmed Wahdan <ahmed.wahdan@outlook.com>
2021-05-30 11:34:14 +0200

    fix download script

--- script.sh
+++ script.sh
@@ -64,8 +64,8 @@ download_from_s3() {
    fi
 
    echo "Downloading ${DEST_FILE} from S3(${S3_ENDPOINT})..."
-
-    aws $AWS_ARGS s3 get s3://$S3_BUCKET/$S3_PREFIX/$DEST_FILE "/tmp/$DEST_FILE"
+    echo "aws $AWS_ARGS s3 cp - s3://$S3_BUCKET/$S3_PREFIX/$DEST_FILE "/tmp/$DEST_FILE""
+    aws $AWS_ARGS s3 cp - s3://$S3_BUCKET/$S3_PREFIX/$DEST_FILE "/tmp/$DEST_FILE"
 
    if [ $? != 0 ]; then
       echo >&2 "Error downloading ${DEST_FILE} from S3"
